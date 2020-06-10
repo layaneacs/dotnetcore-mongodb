675153dc715fe9cca6d6eb08690263b709659698
layane.andreia@hotmail.com <layane.andreia@hotmail.com>
2020-06-10 15:46:47 -0300

    iniciando a aplicação - criando a entidade produto

---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace dotnetcore.mongo.Models
+{
+    public class Produto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        [BsonElement("Produto")]
+        public string ProdutoNome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
